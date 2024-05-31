0fc2ad27a4955f4e8583253da587c547e3afa0b5
Fernando Batagin Jr <fbatag@gmail.com>
2024-05-31 13:11:31 -0300

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,9 @@ import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +22,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlyeats-fer","appId":"1:944098589096:web:852ec1b5a8d5bb25f9059d","storageBucket":"friendlyeats-fer.appspot.com","locationId":"us-central","apiKey":"AIzaSyDjUFkH2s3Jkz6rm80sPzwbvnbAtHsLnGo","authDomain":"friendlyeats-fer.firebaseapp.com","messagingSenderId":"944098589096","measurementId":"G-QEN16HQXHC"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig())
   ],
 };
